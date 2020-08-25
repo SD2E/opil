870f9f39106b93479b67e97f369cd19050455c4e
Rich Markeloff <richard.markeloff@raytheon.com>
2020-08-25 13:34:40 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup(name='MyRdflib',
-      description='RDF package for CREATE',
-      packages=['rdf_create'],
+setup(name='PyOpil',
+      description='Python package for demonstrating OPIL',
       version='0.0.1',
       install_requires=[
-            'rdflib>=5.0.0'
+            'rdflib>=4.2.2'
             'rdflib-jsonld>=0.5.0'
             'sparqlwrapper>=1.8.5'
       ]
