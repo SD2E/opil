fafebc72fe6c915054ea9567b4dd48ae6cd1d2ee
Bryan Bartley <bryan.a.bartley@raytheon.com>
2020-10-22 12:30:24 -0400

    Bump version to 1.0a2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='opil',
       description='Python package for demonstrating OPIL',
-      version='1.0a1.post1',
+      version='1.0a2',
       install_requires=[
             'sbol3>=1.0a2',
             'rdflib>=5.0.0',
