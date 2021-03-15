c9d22ffa41eb95639f4dad07448089a418431b80
Bryan Bartley <bryan.a.bartley@raytheon.com>
2021-03-14 21:12:35 -0400

    Refactor __init__

--- opil/__init__.py
+++ opil/__init__.py
@@ -1,12 +1,12 @@
-from sbol_factory import SBOLFactory, Document, ValidationReport
+from sbol_factory import SBOLFactory, Document, ValidationReport, UMLFactory
 from .generate_opil_from_strateos import StrateosOpilGenerator
 import sbol3 as sbol
 import os
 import posixpath
 
 # Import ontology
-MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
-default_ontology = posixpath.join(MODULE_PATH, 'rdf/opil.ttl')
-opil_path = posixpath.join(os.path.dirname(os.path.realpath(__file__)), 'rdf/opil.ttl')
-SBOLFactory(locals(), opil_path, 'http://bioprotocols.org/opil/v1#')
-
+__factory__ = SBOLFactory(locals(), 
+                          posixpath.join(os.path.dirname(os.path.realpath(__file__)),
+                                         'rdf/opil.ttl'),
+                          'http://bioprotocols.org/opil/v1#')
+__umlfactory__ = UMLFactory(__factory__)
