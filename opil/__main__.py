7644c12b780c59d6fe95bc799ffce3d417c9cd45
Bryan Bartley <bryan.a.bartley@raytheon.com>
2021-03-25 20:26:24 -0400

    Remove executable module feature

--- opil/__main__.py
+++
@@ -1,8 +0,0 @@
-import os
-import posixpath
-
-# Import ontology
-MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
-default_ontology = posixpath.join(MODULE_PATH, 'rdf/opil.ttl')
-opil_path = posixpath.join(os.path.dirname(os.path.realpath(__file__)), 'rdf/opil.ttl')
-UMLFactory(__factory__, 'http://bioprotocols.org/opil/v1#')
