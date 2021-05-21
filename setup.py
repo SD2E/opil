52e3ef9b249199904ac02a610be26133819d1770
Bryan Bartley (local) <bbartley@ma14146bbartley.ad.bbn.com>
2021-05-21 17:06:30 -0400

    Support visitor

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ from setuptools import setup
 
 setup(name='opil',
       description='Open Protocol Interface Language (OPIL)',
-      version='1.0b5.post1',
+      version='1.0b6',
       install_requires=[
-            'sbol3==1.0a8',
+            'sbol3==1.0a9',
             'rdflib>=5.0.0',
             'rdflib-jsonld>=0.5.0',
             'sparqlwrapper>=1.8.5',
             'pyshacl>=0.13.3',
             'python-dateutil>=2.8.1',
-            'sbol_factory==1.0a3',
+            'sbol_factory==1.0a4',
             'requests',
             'graphviz'
       ],
--- test/test_opil.py
+++ test/test_opil.py
@@ -27,7 +27,7 @@ class TestOpil(unittest.TestCase):
         doc.add(p)
         validation_report = doc.validate()
         self.assertTrue(validation_report.is_valid)
-
+    
     def test_invalid(self):
         # Parameters are forbidden to have more than one name
         doc = Document()
@@ -42,7 +42,7 @@ class TestOpil(unittest.TestCase):
         i_message = validation_report.results.find('Message: ') + 9
         message = validation_report.results[i_message:]
         self.assertEqual(validation_report.__repr__(), message)
-
+    
     def test_required_parameters(self):
         # Confirm that SHACL validation will catch the following rule:
         # A Parameter with required == True must have ParameterValue specified
@@ -51,21 +51,21 @@ class TestOpil(unittest.TestCase):
         p = Parameter()
         p.required = True
         protocol.has_parameter = [p]
-
+    
         er = ExperimentalRequest('er')
         v = ParameterValue()
         er.has_parameter_value = [v]
-
+    
         er.instance_of = protocol
         doc.add(protocol)
         doc.add(er)
         validation_report = doc.validate()
         self.assertFalse(validation_report.is_valid)
-
+    
         v.value_of = p
         validation_report = doc.validate()
         self.assertTrue(validation_report.is_valid)
-
+    
     def test_top_level(self):
         # See issue 38
         doc = Document()
@@ -80,10 +80,10 @@ class TestOpil(unittest.TestCase):
         doc2.read_string(doc.write_string('nt'), 'nt')
         object_ids = [o.identity for o in doc2.objects]
         self.assertIn(protocol.identity, object_ids)
-
+    
         # The following failed prior to resolution of 38
         self.assertNotIn(param.identity, object_ids)
-
+    
     def test_rdf_files(self):
         # In order for this to pass, the TestER graph needs to be merged
         # with the TimeSeries ProtocolInterface graph
@@ -93,73 +93,73 @@ class TestOpil(unittest.TestCase):
         validator = ShaclValidator()
         is_valid, g_results, results = validator.validate(g)
         self.assertTrue(is_valid)
-
+    
     def test_opil_files(self):
         doc = Document()
         doc.read(os.path.join(TEST_FILES, 'opil_output.xml'), 'xml')
         report = doc.validate()
         print(report.message)
         self.assertTrue(report.is_valid)
-
+    
     def test_growth_curve(self):
         with open(os.path.join(TEST_FILES, 'GrowthCurve.json')) as f:
             document_dict = json.load(f)
         protocol_params = document_dict['inputs']
         s = StrateosOpilGenerator()
         doc = s.parse_strateos_json('http://strateos.com',
-                                         document_dict['name'], 
+                                         document_dict['name'],
                                          document_dict['id'],
                                          document_dict['inputs'])
         report = doc.validate()
         self.assertTrue(report.is_valid)
-
+    
     def test_time_series(self):
         with open(os.path.join(TEST_FILES, 'TimeSeriesHTP.json')) as f:
             document_dict = json.load(f)
         s = StrateosOpilGenerator()
         doc = s.parse_strateos_json('http://strateos.com',
-                                         document_dict['name'], 
+                                         document_dict['name'],
                                          document_dict['id'],
                                          document_dict['inputs'])
         protocol = doc.find('http://strateos.com/TimeSeriesHTP')
         dotnames = []
         for param in protocol.has_parameter:
             dotnames.append(param.dotname)
-
+    
         # Confirm that options that contain nested JSON objects are being handled
         self.assertIn('induction_info.induction_reagents.inducer_layout',
                       dotnames)
-
+    
         # Confirm that multiple instances with the same dotname are not created, see #56
         # (These indicate correct handling of `options` objects in the JSON schema)
         self.assertEqual(dotnames.count(
                          'induction_info.induction_reagents.inducer_layout.inducer_unit'), 0)
-
+    
         # Confirm that Strateos dotname encodes options syntax, see #76
         self.assertIn('induction_info.induction_reagents.inducer_layout.|.full_plate.inducer_unit',
                       dotnames)
         self.assertIn('induction_info.induction_reagents.inducer_layout.|.select_cols.inducer_unit',
                       dotnames)
-
+    
         report = doc.validate()
         self.assertTrue(report.is_valid)
-
+    
     def test_obstacle_course(self):
         with open(os.path.join(TEST_FILES, 'ObstacleCourse.json')) as f:
             document_dict = json.load(f)
         s = StrateosOpilGenerator()
         doc = s.parse_strateos_json('http://strateos.com',
-                                         document_dict['name'], 
+                                         document_dict['name'],
                                          document_dict['id'],
                                          document_dict['inputs'])
         report = doc.validate()
         self.assertTrue(report.is_valid)
-
+    
         # Confirm all top level objects in Document are treated like
         # extension objects with the additional top level RDF typee
         for o in doc.objects:
             self.assertIn(SBOL_TOP_LEVEL, o._rdf_types)
-
+    
         # Confirm that JSON objects containing both a `magnitude` and `unit` object
         # are collapsed to create a single MeasureParameter object. See #159
         m = doc.find('http://strateos.com/ObstacleCourse/MeasureParameter6')
@@ -167,27 +167,27 @@ class TestOpil(unittest.TestCase):
                          'reagent_info.kill_switch.|.true.kill_switch_final_conc')
         self.assertEqual(m.default_value.has_measure.unit,
                          'http://www.ontology-of-units-of-measure.org/resource/om-2/millimolair')
-
-
+    
+    
     def test_cell_free_bioswitches(self):
         with open(os.path.join(TEST_FILES, 'CellFreeRiboswitches.json')) as f:
             document_dict = json.load(f)
         s = StrateosOpilGenerator()
         doc = s.parse_strateos_json('http://strateos.com',
-                                         document_dict['name'], 
+                                         document_dict['name'],
                                          document_dict['id'],
                                          document_dict['inputs'])
         protocol = doc.find('http://strateos.com/CellFreeRiboswitches')
         report = doc.validate()
         self.assertTrue(report.is_valid)
-
+    
         # Make sure `value_of` back-pointer is set
         # (This issue was due to StrateosOpilGenerator not setting the value_of,
-        # and is not unique to this schema) 
+        # and is not unique to this schema)
         for p in protocol.has_parameter:
             if p.default_value:
                 self.assertEqual(p.default_value.value_of, p.identity)
-
+    
     def test_measurement(self):
         # Confirm functionality of Measurement and MeasurementType part of data model
         doc = Document()
@@ -208,7 +208,7 @@ class TestOpil(unittest.TestCase):
         m.time = [ Measure(50, 'http://www.ontology-of-units-of-measure.org/resource/om-2#minute') ]
         doc.add(e)
         doc.add(p)
-
+    
     def test_positional_arguments(self):
         # Some SBOL core classes have positional arguments in their constructor.
         # Instantiating OPIL subclasses of these was failing because OPIL did not
@@ -219,55 +219,55 @@ class TestOpil(unittest.TestCase):
         doc.add(p)
         template = Component('design', 'http://foo.org/bar')
         doc.add(template)
-
+    
         # Confirm arbitrary arguments list (positional) and keywords work
         sample_space = SampleSet('conditions', template, name='foo')
         with self.assertRaises(TypeError):
             sample_space = SampleSet('conditions')  # Missing required positional argument `template`
-
+    
         # Confirm keyword arguments work
         self.assertEqual(sample_space.name, 'foo')
         doc.add(sample_space)
-
+    
         p.sample_sets = [sample_space]
         doc2 = Document()
         doc2.read_string(doc.write_string('turtle'), 'turtle')
         sample_space = isinstance(doc2.find(sample_space.identity), TopLevel)
         self.assertTrue(compare_documents(doc, doc2))
-
+    
     def test_is_top_level(self):
         self.assertTrue(__factory__.query.is_top_level('http://bioprotocols.org/opil/v1#SampleSet'))
         self.assertTrue(__factory__.query.is_top_level('http://sbols.org/v3#TopLevel'))
         self.assertFalse(__factory__.query.is_top_level('http://sbols.org/v3#Identified'))
-
+    
     def test_required(self):
         # Confirm that required arguments are propagated from super constructors, i.e., in this
         # example `template` is propagated from CombinatorialDerivation constructor.
         # Also, because this is a TopLevel, `identity` should be the first required argument
         required = __factory__.query.query_required_properties('http://bioprotocols.org/opil/v1#SampleSet')
         self.assertEqual(required, ['identity', 'template', 'replicates'])
-
+    
         required = __factory__.query.query_required_properties('http://bioprotocols.org/opil/v1#EnumeratedValue')
         # non-TopLevel should not have identity as a required property
         self.assertNotIn('identity', required)
-
+    
         # Because the superclass of EnumeratedValue, ParameterValue, also has a restriction
         # on `value_of` property, it was appearing twice in the list of required arguments
         self.assertNotEqual(required.count('value_of'), 2)
-
+    
         doc = Document()
         er = ExperimentalRequest('er')
         doc.add(er)
         mv = MeasureValue()
         mv.has_measure = Measure(100, 'foo')
         er.has_parameter_value = [mv]
-
+    
         # results = doc.validate()
         # print(results.is_valid)
         # print(results.message)
         # with self.assertRaises(TypeError):
         #     m = MeasureValue()
-
+    
     def test_inheritance(self):
         sp = StringParameter()
         self.assertNotIn('http://bioprotocols.org/opil/v1#Parameter', sp._rdf_types)
@@ -278,7 +278,7 @@ class TestOpil(unittest.TestCase):
         self.assertNotIn('http://sbols.org/v3#CombinatorialDerivation', s._rdf_types)
         self.assertIn('http://bioprotocols.org/opil/v1#SampleSet', s._rdf_types)
         self.assertEqual(len(s._rdf_types), 2)
-
+    
     def test_name(self):
         # # Test name is in the opil namespace, not the sbol namespace
         # p = Parameter()
@@ -287,7 +287,7 @@ class TestOpil(unittest.TestCase):
         # p.bar = TextProperty(p, 'http://bioprotocols.org/opil/v1#name', 0, 1)
         # self.assertIsNone(p.foo)
         # self.assertEqual(p.bar, 'foo')
-
+    
         # Test SHACL validation requiring Parameter has name
         doc = Document()
         protocol = ProtocolInterface('pro')
@@ -300,6 +300,38 @@ class TestOpil(unittest.TestCase):
         report = doc.validate()
         self.assertTrue(report.is_valid)
 
+    def test_visitor(self):
+
+        # Define a visitor class to test
+        class Visitor:
+
+            def visit_protocolinterface(self, p):
+                p.description = 'Visited'
+
+            def visit_parameter(self, p):
+                p.description = 'Visited'
+
+        doc = Document()
+        protocol = ProtocolInterface('pro')
+        param = Parameter()
+        measurement_type = MeasurementType()
+        protocol.has_parameter = [param]
+        protocol.measurement_type = [measurement_type]
+        doc.add(protocol)
+
+        self.assertIsNone(protocol.description)
+        self.assertIsNone(param.description)
+        self.assertIsNone(measurement_type.description)
+
+        v = Visitor()
+        protocol.accept(v)
+        param.accept(v)
+        self.assertEqual(protocol.description, 'Visited')
+        self.assertEqual(param.description, 'Visited')
+        self.assertIsNone(measurement_type.description)
+
+
+        
 def compare_documents(doc1, doc2):
     # Now compare the graphs in RDF
     g1 = doc1.graph()
